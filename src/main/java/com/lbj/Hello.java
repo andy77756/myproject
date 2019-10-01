fa846cb62638aead264b4c55e5a0a610ac976d3a
BJ <andy77756@hotmail.com>
2019-10-01 16:15:17 +0800

    Initial commit
    feature1 finish

---
+++ src/main/java/com/lbj/Hello.java
@@ -0,0 +1,9 @@
+package com.lbj;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        Person p = new Person();
+        p.hello();
+    }
+}
